e5a2322b1300af87fa9e3a70efb8d5a92f98feeb
Juanjo098 <89929790+Juanjo098@users.noreply.github.com>
2022-10-12 22:05:19 -0500

    Graphic interface V1.0 created

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,7 +3,7 @@ package main;
 public class Main {
 
     public static void main(String[] args) {
-        String[][] table = fm.ReadSpreadsheet.SpreadsheetTo2dArray();
+        String[][] table = filemanagment.ReadSpreadsheet.SpreadsheetTo2dArray();
         for (String[] strings : table) {
             for (String string : strings) {
                 System.out.print(string + "\t");
