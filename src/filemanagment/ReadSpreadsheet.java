e955bccf1ca89c5a71cc6dcfad2d67860ca8f5fb
Juanjo098 <89929790+Juanjo098@users.noreply.github.com>
2022-10-18 12:23:52 -0500

    Table update

--- src/filemanagment/ReadSpreadsheet.java
+++ src/filemanagment/ReadSpreadsheet.java
@@ -3,13 +3,14 @@ package filemanagment;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 
 public class ReadSpreadsheet {
-    
+
     // Dependencies: POI | HSSF Workbook/Sheet/Row/Cell
     // This method will read and return Excel data into a double array
     public static String[][] SpreadsheetTo2dArray() {
@@ -20,6 +21,10 @@ public class ReadSpreadsheet {
             FileInputStream xlfile = new FileInputStream(file);
             HSSFWorkbook xlwb = new HSSFWorkbook(xlfile);
             HSSFSheet xlSheet = xlwb.getSheetAt(0);
+
+            ArrayList<String[]> array = new ArrayList<>();
+
+            String cad = null;
 
             // Get the number of rows and columns
             int numRows = xlSheet.getLastRowNum() + 1;
@@ -35,10 +40,16 @@ public class ReadSpreadsheet {
                 HSSFRow xlRow = xlSheet.getRow(i);
                 for (int j = 0; j < numCols; j++) {
                     HSSFCell xlCell = xlRow.getCell(j);
-                    if (xlCell != null)
-                        dataTable[i][j] = xlCell.toString();
-                    else
-                        dataTable[i][j] = null;
+
+                    cad = xlCell.toString();
+                    
+                    System.out.println(cad);
+
+                    if (cad.indexOf(".") != -1) {
+                        cad = cad.substring(0, xlCell.toString().indexOf("."));
+                    }
+
+                    dataTable[i][j] = cad;
                 }
             }
         } catch (IOException e) {
